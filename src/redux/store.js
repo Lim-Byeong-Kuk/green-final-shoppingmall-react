30854be6f68ffa7cb0036091923c7db7d09ebc50
ShinSora <cogmo18@gmail.com>
2025-11-24 17:36:22 +0900

    [프론트] 리뷰 등록, 수정, 삭제 기능 구현
    - 리뷰 Api 추가
    - 리뷰 리덕스 삭제
    - 리뷰 등록, 수정, 삭제 기능 구현

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,14 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import cartReducer from "./slices/features/cart/cartSlice";
 import userSlice from "./slices/features/user/userSlice";
-import reviewRatingSlice from "./slices/features/review/reviewRatingSlice";
-import reviewSlice from "./slices/features/review/reviewSlice";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
     userSlice: userSlice,
-    reviewRatingSlice: reviewRatingSlice,
-    reviewSlice: reviewSlice,
   },
 });
